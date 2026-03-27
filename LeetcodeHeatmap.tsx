f9816d8cd82719a56a693fa1d298d282c19c06be
murthy0x05 <murthy0x05@gmail.com>
2026-03-27 23:56:25 +0530

    REBASE(OLD_VERSION): Reverting to old version.

--- LeetcodeHeatmap.tsx
+++
@@ -1,4 +0,0 @@
-// Updated code
-const generateDateKey = (d) => `${d.getUTCFullYear()}-${String(d.getUTCMonth() + 1).padStart(2, '0')}-${String(d.getUTCDate()).padStart(2, '0')}`;
-
-// other code...
